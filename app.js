d1969072819b718ba42eab469deb57a2ea592633
Matthieu Bleichner <matthieu258@hotmail.com>
2024-10-31 14:58:31 +0100

    use cors package

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 const express = require('express');
-// const { seed, update } = require('./scripts/seed');
+const { seed, update } = require('./scripts/seed');
 const marketsRoutes = require('./routes/markets');
 const usersRoutes = require('./routes/users');
+const cors = require('cors');
+
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-  );
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
+app.use(cors());
 app.use('/api', marketsRoutes);
 app.use('/user', usersRoutes);
 
-// app.post('/api/seed', () => {
-//   return seed();
-// });
+app.post('/api/seed', () => {
+  return seed();
+});
 
 module.exports = app;
